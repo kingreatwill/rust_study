a9139841cfb29b836087e239a0bdd8b504d46363
kingreatwill <350840291@qq.com>
2020-09-30 16:36:06 +0800

    模板.

---
+++ cratelib/examples/template.rs
@@ -0,0 +1,14 @@
+/*
+https://rust.libhunt.com/categories/1623-template-engine
+
+https://github.com/Keats/tera
+https://github.com/djc/askama
+https://github.com/lambda-fairy/maud
+https://github.com/sunng87/handlebars-rust
+https://github.com/Stebalien/horrorshow-rs
+https://github.com/kaj/ructe
+https://github.com/utkarshkukreti/markup.rs
+*/
+fn main(){
+
+}
